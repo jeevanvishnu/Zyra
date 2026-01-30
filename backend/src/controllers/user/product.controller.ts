560ff01eccee5ecba452bb7a092cf70576463f43
jeevanvishnu <jeevanvishnu18@gmail.com>
2026-01-30 14:02:56 +0530

    reomove some bugs

--- backend/src/controllers/user/product.controller.ts
+++ backend/src/controllers/user/product.controller.ts
@@ -14,3 +14,21 @@ export const getFeatureProduct = async (req:Request , res:Response) =>{
         res.status(500).json({message:"Internal server error"})
     }
 }
+
+ export const allProducts = async (req:Request , res:Response)=>{
+    try {
+        const {category} = req.query
+        if(!category){
+            res.status(500).json({message:"Internal server error "})
+        }
+        const product = await Products.find({category})
+        if(!product){
+            res.status(404).json({message:"Product not found"})
+        }
+        res.json(product)
+        
+    } catch (error) {
+     console.log("Error is coming from allProducts",error.message)
+      res.status(500).json({message:"Internal server error"})
+    }
+ }
